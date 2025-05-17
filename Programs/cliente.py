81687dc462f5fa361e4f3c1626148c908bd63698
CarlosLvTec <emailaddress@yahoo.com>
2025-05-16 20:11:36 -0600

    Completado

--- Programs/cliente.py
+++ Programs/cliente.py
@@ -1,14 +1,16 @@
 # cliente.py
 import socket
+from llm_testing import translate
 
-SERVER_IP = '192.168.1.200'  # Reemplaza con la IP del servidor
+SERVER_IP = '192.168.1.181'  # Reemplaza con la IP del servidor
 PORT = 12345
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((SERVER_IP, PORT))
     print("Conectado al servidor")
     while True:
-        mensaje = input("Escribe un mensaje: ")
+        prompt = input("Escribe un mensaje: ")
+        mensaje = translate(prompt)
         if mensaje.lower() == 'salir':
             break
         s.sendall(mensaje.encode())
--- Programs/llm_testing.py
+++ Programs/llm_testing.py
@@ -1,13 +1,14 @@
 from ollama import chat
 from ollama import ChatResponse
 
-response: ChatResponse = chat(model='xarm_sorting', messages=[
-  {
-    'role': 'user',
-    'content': 'Place the red squares on top',
-  },
-])
+def translate(text):
+    response: ChatResponse = chat(model='xarm_sorting', messages=[
+    {
+        'role': 'user',
+        'content': text,
+    },
+    ])
 
-real = response['message']['content'].split("</think>").split("/")
-print(real[1])
+    real = response['message']['content'].split("</think>")
+    return(real[1])
 # or access fields directly from the response object
